26d896555a10edf1121ffb7c3376419cbf1b0373
Melissa Longenberger <apple@Irish-iMac.hitronhub.home>
2021-12-10 13:47:23 -0500

    saving small changes

--- notesApp/models.py
+++ notesApp/models.py
@@ -1,3 +1,91 @@
 from django.db import models
+from django.core.validators import RegexValidator
+import re
+from django.db.models.fields import BooleanField, CharField
+from django.db.models.signals import post_save
+from django.contrib.auth.models import User
+from django.db.models.deletion import CASCADE
 
-# Create your models here.
+class UserManager(models.Manager):
+    def validate(self, form):
+        errors = {}
+
+        EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
+        if not EMAIL_REGEX.match(form['email']):
+            errors['email'] = 'Invalid Email Address'
+
+        emailCheck = self.filter(email=form['email'])
+        if emailCheck:
+            errors['email'] = 'Email Address already in use'
+
+        usernameCheck = self.filter(username=form['username'])
+        if usernameCheck:
+            errors['username'] = 'Username already in use'
+
+        if len(form['password']) < 8:
+            errors['password'] = 'Password must be at least 8 characters'
+
+        if form['password'] != form['confirm']:
+            errors['password'] = 'Passwords do not match'
+
+        return errors
+
+class User(models.Model):
+    firstName = models.CharField(max_length=45)
+    lastName = models.CharField(max_length=45)
+    email = models.EmailField(unique=True)
+    username = models.CharField(max_length=45, unique=True)
+    password = models.CharField(max_length=45)
+
+    objects = UserManager()
+
+    userCreatedAt = models.DateTimeField(auto_now_add=True)
+    userUpdatedAt = models.DateTimeField(auto_now=True)
+    def __str__(self):
+        return self.username
+
+class Profile(models.Model):
+    discord = models.CharField(max_length=255, default=None)
+    user = models.OneToOneField(User, unique=True, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='profileImgs', default='bee.jpg')
+    def __str__(self):
+        return f'{self.user.username} Profile'
+
+def create_user_profile(sender, instance, created, **kwargs):
+    
+    if created:
+        User.objects.create(user=instance)
+        post_save.connect(create_user_profile, sender=User)
+
+class Stack(models.Model):
+    stackName = models.CharField(max_length=255)
+
+class Note(models.Model):
+    subject = models.CharField(max_length=255)
+    content = models.TextField()
+    private = models.BooleanField(default=False)
+    upvote = models.IntegerField()
+    user = models.ForeignKey(User, related_name='noteUser', on_delete=CASCADE)
+    stack = models.ForeignKey(Stack, related_name='noteStack', on_delete=CASCADE)
+    def __str__(self):
+        return self.subject
+
+
+class Upload(models.Model):
+    uploadName = models.CharField(max_length=255)
+    upload = models.FileField(upload_to='docs', default='bee.jpg')
+    def __str__(self) -> str:
+        return f'{self.note.subject} Upload'
+
+def create_note_upload(sender, instance, created, **kwargs):
+    if created:
+        Note.objects.create(note=instance)
+        post_save.connect(create_note_upload, sender=Note)
+
+class Comment(models.Model):
+    comment = models.TextField()
+    like = models.IntegerField()
+    user = models.ForeignKey(User, related_name='commentUser', on_delete=CASCADE)
+    note = models.ForeignKey(Note, related_name='commentNote', on_delete=CASCADE)
+    commentCreatedAt = models.DateTimeField(auto_now_add=True)
+    commentUpdatedAt = models.DateTimeField(auto_now=True)
---
+++ notesApp/signals.py
@@ -0,0 +1,23 @@
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from django.contrib.auth.models import User
+from .models import *
+
+
+@receiver(post_save, sender=User)
+def createProfile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+@receiver(post_save, sender=User)
+def saveProfile(sender, instance, **kwargs):
+    instance.profile.save()
+
+@receiver(post_save, sender=Note)
+def createUpload(sender, instance, created, **kwargs):
+    if created:
+        Upload.objects.create(note=instance)
+
+@receiver(post_save, sender=Note)
+def saveUpload(sender, instance, **kwargs):
+    instance.upload.save()
